08b471dbbd5777153905f8391f2cd48417c2c72a
aliriomiguel <aliriomiguel@gmail.com>
2022-06-02 16:27:58 +0200

    added the account and the current account balance to the front end - visibles in the console

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,74 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import './App.css'
 
 class App extends Component {
+
+  async componentWillMount(){
+    await this.loadWeb3()
+    console.log('debug')
+    console.log(window.ethereum)
+    await this.loadBlockchainData()
+  }
+
+  async loadBlockchainData(){
+    const web3 = window.ethereum
+    
+    const accounts = await web3.request({ method: 'eth_requestAccounts' })
+    this.setState({ account: accounts[0] })
+    console.log('account ' + this.state.account)
+    
+
+    const hexBalance = await web3.request({method: 'eth_getBalance', params: [
+      this.state.account,
+      'latest'
+    ], 'id': 1})
+    var dec = parseInt(hexBalance, 16)
+    const ethBalance = dec / (10**18)
+    this.setState({ethBalance})
+    console.log('balance ' + ethBalance)
+  }
+
+  loadWeb3(){
+    window.addEventListener('load', async () => {
+      // Modern dapp browsers...
+      if (window.ethereum) {
+          const web3 = new Web3(window.ethereum);
+          try {
+              // Request account access if needed
+              await window.ethereum.enable();
+              // Acccounts now exposed
+              return web3;
+          } catch (error) {
+              console.error(error);
+          }
+      }
+      // Legacy dapp browsers...
+      else if (window.web3) {
+          const web3 = window.web3;
+          //window.web3 = new Web3(window.web3.currentProvider);
+          console.log('Injected web3 detected.');
+          return web3;
+          // Acccounts always exposed
+      }
+      // Non-dapp browsers...
+      else {
+        const provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+        const web3 = new Web3(provider);
+        console.log('No web3 instance injected, using Local web3.');
+        return web3;
+      }
+  });
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      account : '',
+      ethBalance : '0'
+    }
+  }
+
   render() {
     return (
       <div>
@@ -20,25 +86,7 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Hello World</h1>
               </div>
             </main>
           </div>
