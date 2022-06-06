a08d04707437582f4961dac48ea8b311d228d947
aliriomiguel <aliriomiguel@gmail.com>
2022-06-06 17:16:27 +0200

    fixed the web3 issue, finish linking the sc's to the App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react'
 import Web3 from 'web3'
+import Token from '../abis/Token.json'
+import EthSwap from '../abis/EthSwap.json'
+import Main from './Main'
+import Navbar from './Navbar'
 import './App.css'
+
 
 class App extends Component {
 
+  
   async componentWillMount(){
     await this.loadWeb3()
-    console.log('debug')
-    console.log(window.ethereum)
     await this.loadBlockchainData()
   }
 
   async loadBlockchainData(){
-    const web3 = window.ethereum
     
-    const accounts = await web3.request({ method: 'eth_requestAccounts' })
+    const web3 = window.web3
+    
+    const accounts = await web3.eth.getAccounts()
+    //console.log(accounts)
+    this.setState({account: accounts[0]})
+
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    //console.log(ethBalance)
+    this.setState({ethBalance})
+    
+
+    /* 
+    //A solution that I used since I got the report from MM that they don't inject web3.js anymore
+    const windowEthereum = window.ethereum
+
+    const accounts = await windowEthereum.request({ method: 'eth_requestAccounts' })
     this.setState({ account: accounts[0] })
     console.log('account ' + this.state.account)
     
-
-    const hexBalance = await web3.request({method: 'eth_getBalance', params: [
+    const hexBalance = await windowEthereum.request({method: 'eth_getBalance', params: [
       this.state.account,
       'latest'
     ], 'id': 1})
@@ -27,66 +44,83 @@ class App extends Component {
     const ethBalance = dec / (10**18)
     this.setState({ethBalance})
     console.log('balance ' + ethBalance)
-  }
 
-  loadWeb3(){
-    window.addEventListener('load', async () => {
-      // Modern dapp browsers...
-      if (window.ethereum) {
-          const web3 = new Web3(window.ethereum);
-          try {
-              // Request account access if needed
-              await window.ethereum.enable();
-              // Acccounts now exposed
-              return web3;
-          } catch (error) {
-              console.error(error);
-          }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-          const web3 = window.web3;
-          //window.web3 = new Web3(window.web3.currentProvider);
-          console.log('Injected web3 detected.');
-          return web3;
-          // Acccounts always exposed
-      }
-      // Non-dapp browsers...
-      else {
-        const provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
-        const web3 = new Web3(provider);
-        console.log('No web3 instance injected, using Local web3.');
-        return web3;
-      }
-  });
+    const networkId = await windowEthereum.networkVersion // this was used to get the network Id
+    */
+
+
+    //Load Token
+    const networkId = await web3.eth.net.getId()
+    //console.log(networkId)
+    const tokenData = Token.networks[networkId]
+    if(tokenData){
+      const token = new web3.eth.Contract(Token.abi, tokenData.address)
+      this.setState({ token })
+      //console.log('token contract:')
+      //console.log(token)
+      let tokenBalance = await token.methods.balanceOf(this.state.account).call()
+      //console.log('tokenBalance: ', tokenBalance.toString())
+      this.setState({ tokenBalance: tokenBalance.toString()})
+    }
+    else{
+      window.alert('Token contract not deployed to detected network')
+    }
+
+    //Load EthSwap
+    const ethSwapData = EthSwap.networks[networkId]
+    if(ethSwapData){
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address)
+      this.setState({ ethSwap })
+    }
+    else{
+      window.alert('Token contract not deployed to detected network')
+    }
+    //console.log(this.state.ethSwap)
+  } 
+
+  async loadWeb3(){
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else{
+      window.alert('Non-ethereum browser detected. You should consider trying MetaMask!')
+    }
+
+    this.setState({loading: false})
   }
 
   constructor(props){
     super(props)
     this.state = {
       account : '',
-      ethBalance : '0'
+      token: {},
+      ethSwap: {},
+      tokenBalance: '0',
+      ethBalance : '0',
+      loading: true
     }
   }
 
   render() {
+    let content
+    if(this.state.loading){
+      content = <p id = "loader" className="text-center">Loading...</p>
+    }
+    else{
+      content = <Main/>
+    }
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account = {this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
-                <h1>Hello World</h1>
+                {content}
               </div>
             </main>
           </div>
